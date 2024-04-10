8df79f38b86ab40f76161e52177a7158a2aa4a77
Christian McGhee <chrs.mcghee@gmail.com>
2024-04-10 09:52:19 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html>
-      <head>
-                <title>Welcome Team Typo to the world of Applicvation Deployment</title>title>
-      </head>head>
-      <body>
-                <?php echo '<p>Team Typo Rules!</p>'; ?>
-      </body>body>
-</html>html>
-      </head>
-</html>
+
+<?php echo '<p>Team Typo Rules!</p>'; ?>
+
